275ebe28db070abf5ae29db9e9908e9b55cdf5d2
Ragiax <arthur.haye@etu.univ-poitiers.fr>
2023-03-07 17:24:59 +0100

    Version 3

--- src/Controller/Sandbox/InjectionController.php
+++ src/Controller/Sandbox/InjectionController.php
@@ -36,14 +36,24 @@ class InjectionController extends AbstractController
         return new Response('<body>Injection::un</body>');
     }
 
-    #[Route('/create-flash', name: '_create_flash')] // crée des message flash
+    #[Route('/create-flash', name: '_create_flash')]
     public function createFlashAction(Session $session): Response
     {
         // par exemple cette action supprime une entrée dans la base de données
         $session->getFlashBag()->add('info', 'L\'enregistrement a été supprimé');
         $this->addFlash('info', 'L\'enregistrement a été supprimé (bis repetita)');
+        $this->addFlash('error', 'Il pourrait y avoir une erreur');
+        $this->addFlash('error', 'Il pourrait y avoir une erreur (bis repetita)');
         return $this->redirectToRoute('sandbox_injection_display_flash');
     }
+
+    #[Route('/display-flash', name: '_display_flash')]
+    public function displayFlashAction(): Response
+    {
+        return $this->render('Sandbox/Injection/displayFlash.html.twig');
+    }
+
+
 }
 
 
